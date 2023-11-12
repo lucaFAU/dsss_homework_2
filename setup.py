eb3dc681fabfc7e9f2d6291aeda88b2e47068c50
lucaFAU <74703990+lucaFAU@users.noreply.github.com>
2023-11-12 21:26:32 +0100

    add setup details

--- setup.py
+++ setup.py
@@ -1,1 +1,11 @@
+from distutils.core import setup
+from setuptools import find_packages
 
+setup(
+  name="Math_quiz",
+  version="0.1",
+  author="DSSS",
+  author_email="lucaManzke@fau.de",
+  packages=find_packages(),
+  install_requires=["numpy"],
+)
